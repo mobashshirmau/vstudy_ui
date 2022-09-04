8c608cd843e7452e2f0d782d93a7f36935719ee6
Mobashshir <mobashshirmau@gmail.com>
2022-09-04 18:33:27 +0530

    change navbar content

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { ThrowStmt } from '@angular/compiler';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { CategoryService } from 'src/app/services/category.service';
 import { LoginService } from 'src/app/services/login.service';
 
 @Component({
@@ -11,15 +13,14 @@ import { LoginService } from 'src/app/services/login.service';
 export class NavbarComponent implements OnInit {
   isLoggedIn = false;
   user = null;
+  categories;
 
   @Output() closeSideNav = new EventEmitter();
-
-
 
    onToggleClose() {
     this.closeSideNav.emit();
 }
-  constructor(public login: LoginService, private router: Router) {}
+  constructor(public login: LoginService, private router: Router,private _cat: CategoryService, private _snack: MatSnackBar) {}
 
   ngOnInit(): void {
     this.isLoggedIn = this.login.isLoggedIn();
@@ -28,6 +29,25 @@ export class NavbarComponent implements OnInit {
       this.isLoggedIn = this.login.isLoggedIn();
       this.user = this.login.getUser();
     });
+    this._cat.categories().subscribe(
+      (result: any) => {
+        if(result.status=='success'){
+          this.categories = result.data;
+          
+        }
+        else{
+          this._snack.open(result.message);
+        
+        }
+       
+      },
+      (error) => {
+        this._snack.open('Error in loading categories from server', '', {
+          duration: 3000,
+        });
+      }
+    );
+
   }
 
   public logout() {
