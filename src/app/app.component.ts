c0e2d04dbdaf2c309feb9fd8ba5dd585e699af25
Syed Abdullah <sayed.abdullah@telus.com>
2022-09-29 02:25:49 +0530

    Adds session validation to app-root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,42 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HeaderAuthenticatorService } from './services/header-authenticator.service';
+import baseUrl from './services/helper';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'TestYourself';
+  constructor(private _http: HttpClient, private _headerAuth: HeaderAuthenticatorService, private router: Router, private snack: MatSnackBar,
+
+    ){}
+ checkIfUserSessionIsValid(){
+  this._http.get(`${baseUrl}/session/if-valid`,{headers: this._headerAuth.createAuthorizationHeader()}).subscribe(
+    (result: any) => {
+    return  
+    },
+    (error: any) => {
+      
+      if (error.status==403){
+        localStorage.clear()
+        this.snack.open('Session expired', '', {
+          duration: 3000,
+        });
+        this.router.navigate(['login']);
+        
+
+      }
+    }
+  )
+ }
+ ngOnInit(): void {
+  this.checkIfUserSessionIsValid()
+}
+ 
 }
